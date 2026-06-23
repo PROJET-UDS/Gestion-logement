5149119b175d50792c5c82d028a77f8b1e8dcce8
franc-dev9 <franctalla013@gmail.com>
2026-06-23 13:40:44 +0100

    first commit

---
+++ logement_service/src/main/java/com/immobilierUds/logement_service/LogementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.immobilierUds.logement_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogementServiceApplication.class, args);
+	}
+
+}
---
+++ logement_service/src/test/java/com/immobilierUds/logement_service/LogementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.immobilierUds.logement_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
